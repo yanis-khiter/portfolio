ab3055e10415abba16dfb04073a5096d9b5eb232
yanis-khiter <yanis.khiter@laplateforme.io>
2022-04-08 16:11:53 +0200

    99%

--- assets/js/script.js
+++ assets/js/script.js
@@ -163,23 +163,5 @@ function scroll() {
 });
 
 
-  ///////////////////////////////
-  // google map
-  ///////////////////////////////
-
-function initialize()
-{
-var mapProp = {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP,
-  disableDefaultUI: true,
-  scrollwheel: false
-  };
-var map=new google.maps.Map(document.getElementById("googleMap")
-  ,mapProp);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
 
